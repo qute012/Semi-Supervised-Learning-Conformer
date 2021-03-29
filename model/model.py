b96b01e4dbbc9d5b7bfc9ce6dd32ec393e08da63
DeokJin <33983084+qute012@users.noreply.github.com>
2021-03-30 00:19:18 +0900

    update conformer params

--- model/model.py
+++ model/model.py
@@ -3,14 +3,16 @@ import torch.nn as nn
 class Conformer(nn.Module):
     def __init__(
             self,
+            n_classes,
             in_dim,
-            n_blocks,
-            hidden_dim,
-            num_heads,
+            encoder_dim=256,
+            enc_layers=16,
+            num_heads=4,
             kernel_size=32,
             conv_expansion_factor=2,
             ffn_expansion_factor=4,
-            n_layers=1,
+            decoder_dim=640,
+            dec_layers=1,
             dropout_p=0.1
     ):
         super().__init__()
