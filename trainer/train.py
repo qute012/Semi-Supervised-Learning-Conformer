077f0c5c81e7550fcb1888c3111d9109642f32a3
DeokJin <33983084+qute012@users.noreply.github.com>
2021-03-30 01:25:17 +0900

    Update train.py

--- trainer/train.py
+++ trainer/train.py
@@ -1,1 +1,19 @@
-def deepspeedtrain(conf):
+import deepspeed as ds
+
+class DeepSpeedTrain(object):
+    def __init__(self, conf):
+        self.conf = conf
+        self.model = self.build_model()
+        pass
+
+    def build_model(self):
+        if self.conf['model_type']=='ConformerCTC':
+            from model.model import ConformerCTC as Conformer
+        elif self.conf['model_type']=='ConformerTransducer':
+            from model.model import ConformerTransducer as Conformer
+
+        model = Conformer(self.conf['model_params'])
+
+        pass
+
+    def run(self):
