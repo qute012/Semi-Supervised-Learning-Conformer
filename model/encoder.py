da79f86d96809992a9a68a5304b60552c24f7688
DeokJin <33983084+qute012@users.noreply.github.com>
2021-03-30 12:59:58 +0900

    Update encoder.py

--- model/encoder.py
+++ model/encoder.py
@@ -1,8 +1,25 @@
+import torch
 import torch.nn as nn
 
 from sub_sample import SubSampling
 from block import ConformerBlock
 
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, dim_model, max_length=2000, dropout_p=0.1):
+        super().__init__()
+        pe = torch.zeros(max_length, dim_model, requires_grad=False)
+        position = torch.arange(0, max_length).unsqueeze(1).float()
+        exp_term = torch.exp(torch.arange(0, dim_model, 2).float() * -(math.log(10000.0) / dim_model))
+        pe[:, 0::2] = torch.sin(position * exp_term)
+        pe[:, 1::2] = torch.cos(position * exp_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+        self.dropout = nn.Dropout(dropout_p)
+
+    def forward(self, x):
+        return self.dropout(x + self.pe[:, :x.size(1)])
 
 class ConformerEncoder(nn.Module):
     def __init__(
