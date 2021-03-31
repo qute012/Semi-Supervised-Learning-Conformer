fb17ba568503d555838c2aaf863855d16d804314
DeokJin <33983084+qute012@users.noreply.github.com>
2021-03-31 15:40:13 +0900

    Update block.py

--- model/block.py
+++ model/block.py
@@ -69,25 +69,20 @@ class OptionalSequential(nn.Module):
         opt_params = list(kwargs.keys())
         opt_inputs = list(kwargs.values())
 
-        if len(args) == 1:
-            inputs = args[0]
-        else:
-            inputs = args
-
         for layer in self.layers:
-            params = set(layer.__code__.co_varnames)
+            params = set(layer.forward.__code__.co_varnames)
             opt_lparams = list(set(opt_params) & params)
             opt_linputs = [opt_inputs[opt_params.index(k)] for k in opt_lparams]
             opt = self.make_dict(opt_lparams, opt_linputs)
 
             if len(opt) == 0:
-                inputs = layer(inputs)
+                inputs = layer(*inputs)
             else:
-                inputs = layer(inputs, **opt)
+                inputs = layer(*inputs, **opt)
         return inputs
 
     @staticmethod
-    def make_dict(self, keys, values):
+    def make_dict(keys, values):
         temp_dict = dict()
         for k, v in zip(keys, values):
             temp_dict[k] = v
